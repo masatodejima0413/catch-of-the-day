1afa9665e45261b8cb9f962ebcbd90dcff7d26aa
masatodejima <happyturn0413@gmail.com>
2019-05-06 05:42:29 +0900

    trying state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,14 +5,27 @@ import Order from "./Order";
 
 
 class App extends React.Component {
+    
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        const fishes = { ...this.state.fishes };
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({
+            fishes: fishes
+        });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Flesh Seafook Market" />
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
