87ebf345c479548e3aca15f164efe8190a08d558
masatodejima <happyturn0413@gmail.com>
2019-05-10 22:38:40 +0900

    function completed

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,13 +2,36 @@ import React from 'react';
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
-
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
     
     state = {
         fishes: {},
         order: {}
+    };
+
+    componentDidMount(){
+        const { params } =this.props.match;
+        const localStoratgeRef = localStorage.getItem(params.storeId);
+        if(localStoratgeRef){
+            this.setState({ order: JSON.parse(localStoratgeRef)});
+        }
+        this.ref = base.syncState(`${params.storeId}/fishes`, {
+            context: this,
+            state: "fishes"
+        });
+
+    };
+
+    componentDidUpdate(){
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+    };
+
+    componentWillUnmount(){
+        base.removeBinding(this.ref);
     };
 
     addFish = fish => {
@@ -18,14 +41,61 @@ class App extends React.Component {
             fishes: fishes
         });
     }
+
+    updateFish = (key, updatedFish ) =>{
+        const fishes = { ...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({ fishes });
+    };
+
+    deleteFish = (key) => {
+        const fishes = { ...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
+    };
+
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        });
+    };
+
+    addToOrder = (key) => {
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({order});
+    };
+
+    removeFromOrder = (key) => {
+        const order = {...this.state.order};
+        delete order[key];
+        this.setState({order});
+        console.log(this.state.order);
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Flesh Seafook Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => 
+                        <Fish 
+                            key={key} 
+                            index ={key}
+                            details={this.state.fishes[key]} 
+                            addToOrder={this.addToOrder}
+                        />)}
+                    </ul>
                 </div>
-                <Order />
-                <Inventory addFish={this.addFish} />
+                <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
+                <Inventory 
+                addFish={this.addFish}
+                updateFish={this.updateFish}
+                deleteFish={this.deleteFish}
+                loadSampleFishes={this.loadSampleFishes} 
+                fishes={this.state.fishes}
+                 />
             </div>
         );
     }
